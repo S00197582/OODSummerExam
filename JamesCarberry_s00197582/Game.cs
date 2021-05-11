bfe055a34578453e9d4f194b84184ff3150fb162
James Carberry <S00197582@mail.itsligo.ie>
2021-05-11 09:46:38 +0100

    Added Game class with properties

---
+++ JamesCarberry_s00197582/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamesCarberry_s00197582
+{
+    class Game
+    {
+        public int GameID { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public double price { get; set; }
+        public string Game_Image { get; set; }
+    }
+}
