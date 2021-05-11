7472e47468e0b7911ac33ddddda8444ca4afb501
James Carberry <S00197582@mail.itsligo.ie>
2021-05-11 09:41:03 +0100

    initial commit

---
+++ JamesCarberry_s00197582/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamesCarberry_s00197582
+{
+    class Class1
+    {
+    }
+}
